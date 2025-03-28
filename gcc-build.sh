23854ebffb1febb01d4725676ed286a44fd1ad7d
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2025-03-28 20:17:43 +0900

    Update gcc-build.sh

--- gcc-build.sh
+++ gcc-build.sh
@@ -5,4 +5,4 @@ pacman -Syu --noconfirm base-devel sudo git
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 su builder -c "gpg --recv 38DBBDC86092693E"
-cd ./linux-xanmod ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" env _config=config _compress_modules=y _use_numa=y _use_tracers=n _microarchitecture=92 makepkg --noconfirm -sc"
+cd ./linux-xanmod ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" env _config=config _compress_modules=y _use_numa=y _use_tracers=n makepkg --noconfirm -sc"
