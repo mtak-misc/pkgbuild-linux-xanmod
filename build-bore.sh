86b63077681f83defa209f76c36befa734ff7e66
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-03-26 20:55:18 +0900

    Update build-bore.sh

--- build-bore.sh
+++ build-bore.sh
@@ -5,5 +5,14 @@ pacman -Syu --noconfirm base-devel sudo git
 useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 #chown builder -R ..
 #chown builder -R /tmp
+
+curl -LO curl -L https://archlinux.org/packages/extra-staging/x86_64/llvm/download/ -o llvm-x86_64.pkg.tar.zst
+curl -LO curl -L https://archlinux.org/packages/extra-staging/x86_64/llvm-libs/download/ -o llvm-libs-x86_64.pkg.tar.zst
+curl -LO curl -L https://archlinux.org/packages/extra-staging/x86_64/lld/download/ -o lld-x86_64.pkg.tar.zst
+curl -LO curl -L https://archlinux.org/packages/extra-staging/x86_64/compiler-rt/download/ -o compiler-rt-x86_64.pkg.tar.zst
+curl -LO curl -L https://archlinux.org/packages/extra-staging/x86_64/clang/download/ -o clang-x86_64.pkg.tar.zst
+
+pacman --disable-sandbox --noconfirm -U *.pkg.tar.zst
+
 su builder -c "gpg --recv 38DBBDC86092693E"
 cd ./linux-xanmod-bore ; su builder -c "yes '' | MAKEFLAGS=\"-j $(nproc)\" env _compress_modules=y _use_numa=y _use_tracers=n _use_O3=y _compiler='clang' makepkg --noconfirm -sc"
