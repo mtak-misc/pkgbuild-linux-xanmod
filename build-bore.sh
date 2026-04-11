0b85598339615bcea89f8949a6cb95b87e62c480
mtak-misc <116892362+mtak-misc@users.noreply.github.com>
2026-04-12 07:16:22 +0900

    Update build-bore.sh

--- build-bore.sh
+++ build-bore.sh
@@ -6,12 +6,6 @@ useradd builder -u ${USERID} -m -G wheel && echo "builder ALL=(ALL) NOPASSWD: AL
 #chown builder -R ..
 #chown builder -R /tmp
 
-curl -L https://archlinux.org/packages/extra-staging/x86_64/llvm/download/ -o llvm-x86_64.pkg.tar.zst
-curl -L https://archlinux.org/packages/extra-staging/x86_64/llvm-libs/download/ -o llvm-libs-x86_64.pkg.tar.zst
-curl -L https://archlinux.org/packages/extra-staging/x86_64/lld/download/ -o lld-x86_64.pkg.tar.zst
-curl -L https://archlinux.org/packages/extra-staging/x86_64/compiler-rt/download/ -o compiler-rt-x86_64.pkg.tar.zst
-curl -L https://archlinux.org/packages/extra-staging/x86_64/clang/download/ -o clang-x86_64.pkg.tar.zst
-
 pacman --disable-sandbox --noconfirm -U *.pkg.tar.zst
 
 su builder -c "gpg --recv 38DBBDC86092693E"
